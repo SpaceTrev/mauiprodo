6501c9cb3826a596fb0c3dfe5ec51ad602828831
Space Trev <spacetrev@Spaces-MBP.hsd1.co.comcast.net>
2019-04-26 09:39:28 -0600

    fixed card sizing

--- public/pages/media/js/script.js
+++ public/pages/media/js/script.js
@@ -1,14 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
     var userFeed = new Instafeed({
         get: 'user',
         userId: '9600019397',
         accessToken: '9600019397.1677ed0.72a3a581968d409c84c47180490899ca',
         sortBy: 'most-recent',
-        filter: function(image) {
+        filter: function (image) {
             return image.type === "image";
-          },
-        limit: 15,
+        },
+        limit: function (image) {
+            if (image.type === "image") {
+                return 12;
+            }
+        },
         template: `<div class="container col-lg-3 col-md-4 col-sm-6 col-12 centered instaimg container-img"> <img src="{{image}}" alt="{{caption}}" class="img-fluid border rounded image" style="height: 221px; width: 295px; margin-top: 5%; border-color: #31b0d5 !important"> <div class="rounded overlayd"> <div class="text">{{caption}} </div><div class="button-overlay"><a href="{{link}}" target="_blank" class="btn-gram btn btn-sm btn-outline-dark"><i class="fa fa-instagram"></i></a></div></div></div> `,
         resolution: 'standard_resolution',
     });
